9b1bd3439f26402ea0afa6387bb5ad8891074eb8
Ethmane Didi <ousman.didibela@gmail.com>
2023-06-23 13:08:41 +0200

    Update Navbar component and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Add more configuration options here
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    // Add webpack customizations if needed
+    // For example:
+    if (isServer) {
+      // Perform server-side specific configuration
+    } else {
+      // Perform client-side specific configuration
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
